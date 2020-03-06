e3941ec9bde5ea94de15c1fa409d292d04813f26
marioCarbajalScio <60710502+marioCarbajalScio@users.noreply.github.com>
2020-03-06 16:17:37 -0600

    .Net

---
+++ .NET-MVC-Entity-master/Training/Startup.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Training.API.Contracts;
+using Training.API.Contracts.Services;
+using Training.API.Operations.Users;
+using Training.API.Operations.Products;
+using Training.Data;
+using Training.Data.Repositories;
+using Training.Exceptions;
+using Training.Services;
+using Training.API.Operations.Orders;
+
+namespace Training
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration.GetConnectionString("sqlserver");
+            services.AddDbContext<StoreContext>
+                (options => options.UseSqlServer(connection));
+            services.AddTransient<ErrorHandlingMiddleware>();
+            ConfigureAuthentication(services);
+            ConfigureOperations(services);
+            ConfigureRepositories(services);
+            ConfigureInfrastructureServices(services);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);   
+        }
+
+        public void ConfigureAuthentication(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "bearer";
+            }).AddJwtBearer("bearer", options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["serverSigningPassword"])),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero 
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                        {
+                            context.Response.Headers.Add("Token-Expired", "true");
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        }
+
+
+        public void ConfigureOperations(IServiceCollection services)
+        {
+            services.AddTransient<GetAll>();
+            services.AddTransient<SignUp>();
+            services.AddTransient<SignIn>();
+            services.AddTransient<GetAllProducts>();
+            services.AddTransient<GetAllOrders>();
+            services.AddTransient<AddOrder>();
+            services.AddTransient<GetOrdersByUser>();
+        }
+
+        public void ConfigureInfrastructureServices(IServiceCollection services)
+        {
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+            services.AddScoped<ITokenService, TokenService>();
+        }
+
+        public void ConfigureRepositories(IServiceCollection services)
+        {
+            services.AddTransient<IUsersRepository,UsersRepository>();
+            services.AddTransient<IProductsRepository,ProductsRepository>();
+            services.AddTransient<IOrdersRepository,OrdersRepository>();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseAuthentication();
+            app.UseErrorHandlingMiddleware();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
